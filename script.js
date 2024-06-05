4175311163b1e501c1be08443bae6048318b14cc
Jose Silva <1705.jlss@gmail.com>
2024-06-05 18:11:15 -0400

    se añaden trabajos

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function () {
-    emailjs.init('7o04NQlU2cySHvB1g');
+    emailjs.init('CEjUS8rrtcdM4QzJN');
     
     const form = document.getElementById('contact-form');
     form.addEventListener('submit', function (event) {
@@ -15,33 +14,60 @@ document.addEventListener('DOMContentLoaded', function () {
         const profesion = document.getElementById('profesion').value;
         const rent = document.getElementById('rent').value;
         const experience = document.getElementById('experience').value;
-        const cambioResidencia = document.getElementsByName('cambio-residencia').value;
-        const english = document.getElementsByName('english').value;
-        const file = document.getElementById('file').value;
+        const cambioResidenciaElement = document.querySelector('input[name="cambioResidencia"]:checked');
+        const cambioResidencia = cambioResidenciaElement ? cambioResidenciaElement.value : '';
+        const englishElement = document.querySelector('input[name="english"]:checked');
+        const english = englishElement ? englishElement.value : '';
+        const fileInput = document.querySelector('input[type="file"]');
+        const file = fileInput.files[0]; // Obtener el primer archivo seleccionado
+
+        // Verificar si se ha seleccionado un archivo
+        if (file) {
+            // Crear un FileReader
+            const reader = new FileReader();
+            
+            // Configurar el evento onload para leer el contenido del archivo cuando esté disponible
+            reader.onload = function (event) {
+                const fileData = event.target.result; // Contenido del archivo en base64
+                sendEmail(code, rol, name, lastname, email, comuna, profesion, rent, experience, cambioResidencia, english, fileData);
+            };
+
+            // Leer el contenido del archivo como base64
+            reader.readAsDataURL(file);
+        } else {
+            // Si no se selecciona ningún archivo, enviar el formulario sin el archivo adjunto
+            sendEmail(code, rol, name, lastname, email, comuna, profesion, rent, experience, cambioResidencia, english, null);
+        }
+    
+
         
-        const templateParams = {
-            code: code,
-            rol: rol,
-            name: name,
-            lastname: lastname,
-            email: email,
-            comuna: comuna,
-            profesion: profesion,
-            rent: rent,
-            experience: experience,
-            cambioResidencia: cambioResidencia,
-            english: english,
-            file: file
-        };
-        
-        emailjs.send('service_z00tgz7', 'template_hbbqas9', templateParams)
-            .then(function (response) {
-                console.log('SUCCESS!', response.status, response.text);
-                document.getElementById('status').innerHTML = 'Hemos enviado tu postulación';
-                form.reset();
-            }, function (error) {
-                console.log('FAILED...', error);
-                document.getElementById('status').innerHTML = 'Tuvimos un error al enviar el mensaje';
-            });
+        // Función para enviar el formulario con EmailJS
+        function sendEmail(code, rol, name, lastname, email, comuna, profesion, rent, experience, cambioResidencia, english, fileData) {
+            const templateParams = {
+                code: code,
+                rol: rol,
+                name: name,
+                lastname: lastname,
+                email: email,
+                comuna: comuna,
+                profesion: profesion,
+                rent: rent,
+                experience: experience,
+                cambioResidencia: cambioResidencia,
+                english: english,
+                file: fileData // Enviar el contenido del archivo como base64
+            };
+            
+            emailjs.send('service_phqxof8', 'template_agzjuwy', templateParams).then(function (response) {
+                    console.log('SUCCESS!', response.status, response.text);
+                    document.getElementById('status').innerHTML = 'Hemos enviado tu postulación';
+                    console.log(templateParams);
+                    form.reset();
+                })
+                .catch(function (error) {
+                    console.log('FAILED...', error);
+                    document.getElementById('status').innerHTML = 'Tuvimos un error al enviar el mensaje';
+                });
+        }
     });
 });
